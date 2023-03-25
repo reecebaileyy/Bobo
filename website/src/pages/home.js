da8e4443857b17d7a493f2f81af5d26854065721
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-03-24 23:44:12 -0700

    Breakpoints

--- website/src/pages/home.js
+++ website/src/pages/home.js
@@ -2,28 +2,38 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link";
 import headgif from '../../public/assets/spinhead.gif'
-import BoboVision from '../../public/assets/BoboVision_V3.png'
+import BoboVision from '../../public/assets/BoboVisionheader.png'
 
 export default function Home() {
 
   return (
     <>
 
-      <Head>
+    <Head>
         <title>Bobo</title>
         <meta name="description" content="Its all Bobo" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico"/>
-      </Head>
+    </Head>
       
-      <div className='overflow-hidden'>
-        <div class='bg-gray-300 flex flex-col items-center justify-center tv-border'>
-        <Image className='mt-16' src={BoboVision} width={800} height={800}></Image>
-        <div>
-            <h1 className='font-bold font-pressStart'>What should i put here?</h1>
+    <div className='overflow-hidden'>
+        <div class='bg-gray-300 flex items-start justify-start tv-border'>
+            <div className='fixed w-4/5 h-4/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between'>
+                <Link href="/">
+                    <Image className='' src={BoboVision} width={500} height={500}></Image>
+                </Link>
+                
+                <div className='absolute inset-x-0 bottom-0 py-10 h-4/5 grid grid-cols-4 gap-4 bg-green-500'>
+                    <div className='bg-blue-500'></div>
+                    <div className='bg-blue-500'></div>
+                    <div className='bg-blue-500'></div>
+                    <div className='bg-blue-500'></div>
+                    <div className='bg-blue-500'></div>
+                </div>
+            </div>
         </div>
-          </div>
-      </div>
+    </div>
+
     </>
   )
   
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'sm': {'min': '320px', 'max': '639px'},     // Phones (portrait)
+      'md': {'min': '640px', 'max': '767px'},     // Phones (landscape) / Tablets (portrait)
+      'lg': {'min': '768px', 'max': '1023px'},    // Tablets (landscape) / Laptops
+      'xl': {'min': '1024px', 'max': '1279px'},   // Desktops (sm)
+      '2xl': {'min': '1280px', 'max': '1535px'},  // Desktops (md)
+      '3xl': {'min': '1536px'},    
+    },
     extend: {
       animation: {
         flash: 'flash 1s infinite',
@@ -9,7 +17,7 @@ module.exports = {
       keyframes: {
         flash: {
           '0%, 50%, 100%': { opacity: 1 },
-          '25%, 75%': { opacity: 0 },
+          '25%, 75%': { opsacity: 0 },
         },
       },
       backgroundImage: {
