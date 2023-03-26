5f02b464eb4b6b1e5941c2439cf64ea4220e17f0
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-03-25 17:10:15 -0700

    Web3Modal Update

--- website/src/pages/_app.js
+++ website/src/pages/_app.js
@@ -27,7 +27,13 @@ export default function App({ Component, pageProps }) {
         ethereumClient={ethereumClient} 
         themeVariables={{
           '--w3m-font-family': 'pressStart, cursive',
-          '--w3m-accent-color': '#F5841F'
+          '--w3m-accent-color': '#4a4a4a',
+          // '--w3m-logo-image-url': 'https://bobo-opal.vercel.app/assets/spinhead.gif',
+          '--w3m-text-big-bold-size': '15px',
+          '--w3m-text-small-regular-size': '.6rem',
+          '--w3m-text-xsmall-bold-size': '.4rem',
+          '--w3m-text-xsmall-regular-size': '.5rem',
+          '--w3m-font-weight': '400',
         }}
         />
     </>
--- website/src/pages/home.js
+++ website/src/pages/home.js
@@ -28,9 +28,12 @@ export default function Home() {
     
         <div class='bg-gray-300 flex items-start justify-start tv-border'>
             <div className='fixed w-4/5 h-4/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between'>
-                <Link href="/">
-                    <Image className='' src={BoboVision} width={500} height={500}></Image>
-                </Link>
+                <div className="flex justify-between items-center">
+                    <Link href="/">
+                        <Image className='' src={BoboVision} width={500} height={500}></Image>
+                    </Link>
+                    <Web3Button className="ml-20"/>
+                </div>
                 
                 <div className='grid-container absolute inset-x-0 bottom-10 py-10  h-4/5 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-4 bg-gray-300 overflow-y-auto'>
                     <div className='flex flex-col items-center'>
@@ -51,7 +54,7 @@ export default function Home() {
                     </div>
                     <div className='flex flex-col items-center'>                      
                         <Image src={lore} className='w-44 sm:w-20 md:w-32'/>
-                        <Web3Button/>
+                        <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Lore</h1>
                     </div>
                     <div className='flex flex-col items-center'>
                         <Link href="/" className="flex flex-col items-center">
