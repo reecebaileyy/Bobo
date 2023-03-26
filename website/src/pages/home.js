53a3603de2777d962d352122ab6c4adc1ceeaf23
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-03-25 20:54:12 -0700

    Updated web3 integration

--- website/src/pages/home.js
+++ website/src/pages/home.js
@@ -59,13 +59,6 @@ export default function Home() {
     
         <div className='bg-gray-300 flex items-start justify-start tv-border'>
             <div className='fixed w-4/5 h-4/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between'>
-<<<<<<< Updated upstream
-                <div className="flex justify-between items-center">
-                    <Link className='sm:hidden' href="/">
-                        <Image className='' src={BoboVision} width={500} height={500}></Image>
-                    </Link>
-                    <div className=" flex flex-col items-center">
-=======
                 <div className="sm:hidden flex justify-between items-center">
                     <Link className='' href="/">
                         <Image alt='BoboVision' className='' src={BoboVision} width={500} height={500}></Image>
@@ -78,7 +71,7 @@ export default function Home() {
                         onChange={(e) => {
                             const newMintAmount = parseInt(e.target.value);
                             setMintAmount(newMintAmount);
-                            if (balance == 0) {
+                            if (balance?.() == 0) {
                                 setTotalCost((newMintAmount * 0.008) - 0.008);
 
                             } else{
@@ -88,15 +81,12 @@ export default function Home() {
                     />
 
                     <div className="flex flex-col items-center">
->>>>>>> Stashed changes
                         <Link href="/">
                             <Image alt='BoboVision' className='md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden' src={BoboVision} width={500} height={500}></Image>
                         </Link>
                         <Web3Button className="ml-20"/>
                     </div>
                 </div>
-<<<<<<< Updated upstream
-=======
 
                 <div className='z-10 flex flex-col items-center md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden'>
                     <Link href="/">
@@ -104,7 +94,6 @@ export default function Home() {
                     </Link>
                     <Web3Button className="ml-20"/>
                 </div>
->>>>>>> Stashed changes
                 
                 <div className='grid-container absolute inset-x-0 bottom-10 py-10  h-4/5 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-4 bg-gray-300 overflow-y-auto'>
                     <button className='flex flex-col items-center'
