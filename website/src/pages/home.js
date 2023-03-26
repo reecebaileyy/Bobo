bf26301bb4edfbd9133523cc6afddd5b6f237984
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-03-25 18:32:28 -0700

    Update the responsiveness for mobile

--- website/src/pages/home.js
+++ website/src/pages/home.js
@@ -28,17 +28,25 @@ export default function Home() {
     
         <div class='bg-gray-300 flex items-start justify-start tv-border'>
             <div className='fixed w-4/5 h-4/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between'>
-                <div className="flex justify-between items-center">
-                    <Link className='sm:hidden' href="/">
+                <div className="sm:hidden flex justify-between items-center">
+                    <Link className='' href="/">
                         <Image className='' src={BoboVision} width={500} height={500}></Image>
                     </Link>
-                    <div className=" flex flex-col items-center">
+                    <div className="flex flex-col items-center">
                         <Link href="/">
                             <Image className='md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden' src={BoboVision} width={500} height={500}></Image>
                         </Link>
                         <Web3Button className="ml-20"/>
                     </div>
                 </div>
+
+                <div className='z-10 flex flex-col items-center md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden'>
+                    <Link href="/">
+                        <Image className='' src={BoboVision} width={500} height={500}></Image>
+                    </Link>
+                    <Web3Button className="ml-20"/>
+                </div>
+
                 
                 <div className='grid-container absolute inset-x-0 bottom-10 py-10  h-4/5 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-4 bg-gray-300 overflow-y-auto'>
                     <div className='flex flex-col items-center'>
