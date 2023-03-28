e3b91a0b83d6f6fe121c6f41542e1bdf1ba0d002
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-03-27 23:03:45 -0700

    test

--- website/src/pages/Bobochat.js
+++ website/src/pages/Bobochat.js
@@ -1,108 +1,221 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link";
-import React, { useState, useEffect } from "react";
-import Pusher from "pusher-js";
+import { Web3Button } from '@web3modal/react'
+import { useState, useEffect, useRef } from 'react'
+import { ethers } from 'ethers'
+import { Howl, Howler } from 'howler';
+import { useContractWrite, usePrepareContractWrite, useAccount, usePrepareContractRead, useContractRead } from 'wagmi'
+import ABI from '../abi/BoboABI.json'
+import headgif from '../../public/assets/png_gif/spinhead.gif'
+import BoboVision from '../../public/assets/png_gif/BoboVision2.png'
+import discord from '../../public/assets/png_gif/discord.gif'
+import twitter from '../../public/assets/png_gif/twitter.gif'
+import lore from '../../public/assets/png_gif/lore.gif'
+import protocol from '../../public/assets/png_gif/protocol.gif'
+import profile from '../../public/assets/png_gif/profile.gif'
+import { HiVolumeOff, HiVolumeUp } from 'react-icons/hi';
 
-const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
-    cluster: "us3",
-    encrypted: true,
-});
 
-export default function ChatApp() {
-    const [username, setUsername] = useState("");
-    const [message, setMessage] = useState("");
-    const [messages, setMessages] = useState([]);
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
+export default function Home() {
+
+    //PLAY O PAUSE MUSICA
+    const audioRef = useRef();
+    const { Howl, Howler } = require('howler');
+
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    var sound = new Howl({
+        autoplay: false,
+        loop: false,
+        volume: 0.5,
+        src: ['/assets/audio/nostalgia.mp3']
+    });
+
+    const toglePlay = () => {
+        setIsPlaying(!isPlaying);
+        sound.play()
     };
 
-    const handleMessageChange = (event) => {
-        setMessage(event.target.value);
-    };
+    const togleMute = () => {
+        setIsPlaying(!isPlaying);
+        sound.pause()
+    }
+    
 
-    const handleMessageSend = async () => {
-        if (message !== "") {
-            const newMessage = { username: username, message: message };
 
-            // Send the message to the server
-            try {
-                const response = await fetch("/api/send-message", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(newMessage),
-                });
+    // STORING USERS ADDRESS
+    const { address } = useAccount()
 
-                if (response.ok) {
-                    setMessages((messages) => [...messages, newMessage]);
-                    setMessage("");
-                } else {
-                    console.error("Failed to send message");
-                }
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        }
-    };
+    //CONTRACT ARGUMENTS FOR MINTING
+    const [mintAmount, setMintAmount] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
 
-    useEffect(() => {
-        const channel = pusher.subscribe("chat-channel");
-        channel.bind("new-message", (data) => {
-            setMessages((messages) => [...messages, data]);
-        });
-        return () => {
-            channel.unbind_all();
-            channel.unsubscribe();
-        };
-    }, []);
+    //USE CONTRACT READ
+    const { data } = useContractRead({
+        address: '0x85dDe73b1a3a3a55F9147226D6c8AC07E33BD8C9',
+        abi: ABI,
+        functionName: 'balanceOf',
+        args: [address]
+    })
+    const balance = data?.toNumber()
+
+
+    //USE CONTRACT WRITE
+    const { config } = usePrepareContractWrite({
+        address: '0x85dDe73b1a3a3a55F9147226D6c8AC07E33BD8C9',
+        abi: ABI,
+        functionName: '_mint',
+        args: [mintAmount],
+        overrides: { value: ethers.utils.parseEther(totalCost.toString()) }
+    })
+    const { data: mintData, isSuccess, write: mintNFT } = useContractWrite(config)
 
     return (
         <>
+
             <Head>
-                <title>BoboChat</title>
-                <meta name="description" content="Chat with Bobo and friends" />
+                <title>BoboVision</title>
+                <meta name="description" content="Its all Bobo" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div>
-                <div>
-                    <input
-                        className="border border-gray-400 p-2 rounded w-full mb-4"
-                        type="text"
-                        placeholder="Enter username"
-                        value={username}
-                        onChange={handleUsernameChange}
-                    />
-                    <div>
-                        {messages.map((message, index) => (
-                            <div key={index}>
-                                <span className="font-bold">{message.username}: </span>
-                                <span>{message.message}</span>
-                            </div>
-                        ))}
+
+
+            <div className='bg-gray-300 flex items-start justify-start tv-border'>
+                <div className='fixed w-4/5 h-4/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between'>
+                    <div className="sm:hidden flex justify-between items-center">
+                        <Link className='' href="/">
+                            <Image alt='BoboVision' className='' src={BoboVision} width={500} height={500}></Image>
+                        </Link>
+                        <div className="flex flex-col items-center">
+                            <Link href="/">
+                                <Image alt='BoboVision' className='md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden' src={BoboVision} width={500} height={500}></Image>
+                            </Link>
+                            <Web3Button className="ml-20" />
+                        </div>
                     </div>
-                    <div className="mt-4">
-                        <input
-                            className="border border-gray-400 p-2 rounded w-full"
-                            type="text"
-                            placeholder="Type a message..."
-                            value={message}
-                            onChange={handleMessageChange}
-                        />
+
+                    <div className='z-10 flex flex-col items-center md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden'>
+                        <Link href="/">
+                            <Image alt='BoboVision' className='' src={BoboVision} width={500} height={500}></Image>
+                        </Link>
+                        <Web3Button className="ml-20" />
+                    </div>
+
+                    <div className='grid-container absolute inset-x-0 bottom-10 py-10  h-4/5 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-4 bg-gray-300 overflow-y-auto'>
                         <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
-                            onClick={handleMessageSend}
+                            className="flex flex-col items-center"
+                            onClick={() => mintNFT?.()}
                         >
-                            Send
+                            <div className="w-full flex justify-center">
+                                <Image
+                                    alt="Bobo's Big Ass Cranium"
+                                    src={headgif}
+                                    className="w-44 sm:w-20 md:w-32"
+                                />
+                            </div>
+                            <div className="flex items-center justify-center mt-2">
+                                <input
+                                    className="bg-black rounded-md font-pressStart text-white text-center"
+                                    type="number"
+                                    min="1"
+                                    value={mintAmount}
+                                    style={{
+                                        width: `${Math.max(45, mintAmount.toString().length * 8)}px`,
+                                        padding: '0 4px',
+                                    }}
+                                    onClick={(e) => e.stopPropagation()}
+                                    onChange={(e) => {
+                                        e.stopPropagation();
+                                        const inputValue = e.target.value;
+                                        if (inputValue !== "") {
+                                            const newMintAmount = parseInt(inputValue);
+                                            setMintAmount(newMintAmount);
+                                            if (balance == 0) {
+                                                setTotalCost(newMintAmount * 0.008 - 0.008);
+                                            } else {
+                                                setTotalCost(newMintAmount * 0.008);
+                                            }
+                                        } else {
+                                            setMintAmount(0);
+                                        }
+                                    }}
+                                />
+                                <h1 className="font-pressStart text-3xl sm:text-xl md:text-2xl ml-2">
+                                    Mint
+                                </h1>
+                            </div>
                         </button>
+
+                        {
+                            balance >= 1 ? (
+                                <div className='flex flex-col items-center'>
+                                    <Link href="/profile" className="flex flex-col items-center">
+                                        <Image alt='Da protocol' src={headgif} className='w-44 sm:w-20 md:w-32' />
+                                        <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Profile</h1>
+                                    </Link>
+                                </div>
+                            ) : <div className='flex flex-col items-center'>
+                                <Link href="https://www.opensea.io" target="_blank">
+                                    <Image alt='Da protocol' src={headgif} className='w-44 sm:w-20 md:w-32' />
+                                    <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Profile</h1>
+                                </Link>
+                            </div>
+                        }
+                        <div className='flex flex-col items-center'>
+                            <Link href="https://twitter.com/itsallbobo" target="_blank" className="flex flex-col items-center">
+                                <Image alt="Bobo's Twitter" src={headgif} className='w-44 sm:w-20 md:w-32' />
+                                <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Search</h1>
+                            </Link>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <Link href='/boboChat'>
+                                <Image alt='Da Lore' src={headgif} className='w-44 sm:w-20 md:w-32' />
+                                <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2 text-center'>Chat</h1>
+                            </Link>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <Link href="/" className="flex flex-col items-center">
+                                <Image alt='Da Discord' src={discord} className='w-44 sm:w-20 md:w-32' />
+                                <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Discord</h1>
+                            </Link>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <Link href="/" className="flex flex-col items-center">
+                                <Image alt="Bobo's big ass cranium" src={twitter} className='w-44 sm:w-20 md:w-32' />
+                                <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Twitter</h1>
+                            </Link>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <Link href="/" className="flex flex-col items-center">
+                                <Image alt="Bobo's big ass cranium" src={lore} className='w-44 sm:w-20 md:w-32' />
+                                <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Lore</h1>
+                            </Link>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <Link href="/" className="flex flex-col items-center">
+                                <Image alt="Bobo's Big ass cranium" src={protocol} className='w-44 sm:w-20 md:w-32' />
+                                <h1 className='font-pressStart text-3xl sm:text-xl md:text-2xl mt-2'>Protocol</h1>
+                            </Link>
+                        </div>
                     </div>
+                    <button
+                        className="absolute bottom-0 right-0"
+                        onClick={() => {
+                            setIsPlaying(!isPlaying);
+                        }}
+                    >
+                        {!isPlaying ? (
+                            <HiVolumeOff onClick={toglePlay} />
+                        ) : (
+                            <HiVolumeUp onClick={togleMute} />
+                        )}
+                    </button>
                 </div>
             </div>
         </>
+    )
 
-
-    );
 }
