f8ecb3be1299e24a3e9b77239d76f5a6d762dbba
Rbailey8857 <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-04-14 01:39:55 -0700

    Update [tokenId].js

--- website/src/pages/api/tokens/[tokenId].js
+++ website/src/pages/api/tokens/[tokenId].js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     try {
       const { token } = await getTokenById(parseInt(tokenId, 10));
       console.log('Token from API:', token); 
-      res.status(200).json(token);
+      res.status(200).json({token});
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
