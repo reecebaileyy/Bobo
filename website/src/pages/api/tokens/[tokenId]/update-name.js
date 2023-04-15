6334bc57da8280920c951b026367965bc359cc0c
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-04-15 01:56:55 -0700

    Update update-name.js

--- website/src/pages/api/tokens/[tokenId]/update-name.js
+++ website/src/pages/api/tokens/[tokenId]/update-name.js
@@ -24,7 +24,7 @@ async function updateName(req, res) {
             },
           });
 
-        res.status(200).json({ message: 'Name updated successfully' });
+        res.status(200).json({ message: 'Name updated successfully!' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to update name' });
