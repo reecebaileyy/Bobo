d1682e331f7023feb522163db9f6577e777ed6a5
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-04-15 02:43:28 -0700

    Update tokens.js

--- website/lib/prisma/tokens.js
+++ website/lib/prisma/tokens.js
@@ -16,31 +16,19 @@ export async function getTokens() {
 export async function getTokenById(tokenId) {
     try {
       console.log('Searching for tokenId:', tokenId);
-  
-      // Try to get the data from cache
-      const cacheKey = `token-${tokenId}`;
-      const cacheData = await getAsync(cacheKey);
-  
-      if (cacheData) {
-        console.log('Found token in cache:', cacheData);
-        return JSON.parse(cacheData);
-      }
-  
-      // If not found in cache, query the database
-      const token = await prisma.metadatas.findMany({
+      const token = await prisma.metadatas.findFirst({
         where: {
           token: tokenId,
         },
+        select: {
+          metadata: true,
+        },
       });
-  
       console.log('Found token:', token);
-  
-      // Store the result in cache for future use
-      await setAsync(cacheKey, JSON.stringify(token));
-  
       return token;
     } catch (error) {
       console.error(error);
       return { error };
     }
   }
+
