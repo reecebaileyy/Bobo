a65ddd11e902187c9f188b9d40d57a26b189b2b2
Reece Bailey <80913719+reecebaileyy@users.noreply.github.com>
2023-12-26 03:53:28 -0800

    Update

--- website/src/pages/chat.js
+++ website/src/pages/chat.js
@@ -38,6 +38,8 @@ export default function Chat() {
     // STORING USERS ADDRESS/ENS AS USERNAME
     const { address } = getAccount() ?? {};
     const [username, setUsername] = useState('Brokie');
+    const [newUsername, setNewUsername] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const usernameRef = useRef(username); // Create a ref to store the username
     const [message, setMessage] = useState('');
     console.log('account:', address); // Log the account value here
@@ -47,10 +49,27 @@ export default function Chat() {
     useEffect(() => {
         setUsername(address ? address : 'Brokie');
         usernameRef.current = address ? address : 'Brokie';
-
     }, [address]);
 
-    
+    useEffect(() => {
+        usernameRef.current = username;
+    }, [username]);
+
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    const handleUsernameChange = (e) => {
+        setNewUsername(e.target.value);
+    };
+
+    const handleUsernameSubmit = (e) => {
+        e.preventDefault();
+        setUsername(newUsername); // Update the username state
+        usernameRef.current = newUsername; // Update the username ref
+        toggleModal();
+    };
 
     function getUsernameClassName() {
         const balance = balanceOf?.toNumber();
@@ -88,7 +107,7 @@ export default function Chat() {
         console.log("Updated class name:", newUsernameClassName);
         console.log("New class name:", usernameClassName);
     });
-    
+
     useEffect(() => {
         console.log("NEW class name:", usernameClassName);
     }, [usernameClassName]);
@@ -103,7 +122,7 @@ export default function Chat() {
 
         const userDiv = document.createElement('div');
         userDiv.textContent = username;
-        
+
         userDiv.className = `font-pressStart text-xs mb-1 ${usernameClassName}`;
 
         const textDiv = document.createElement('div');
@@ -123,13 +142,13 @@ export default function Chat() {
 
     // CHAT SERVER LOGIC
     const socketRef = useRef(); // Add this line to create a ref for the socket
-    
+
     useEffect(() => {
         socketRef.current = io('https://bobo-chat.herokuapp.com/');
         socketRef.current.on('receive-message', ({ username, message, colorClass }) => { // Change to colorClass
             displayMessage(username, message, colorClass); // Pass the colorClass variable here
         });
-    
+
         // Clean up the socket connection when the component unmounts
         return () => {
             socketRef.current.disconnect();
@@ -139,7 +158,7 @@ export default function Chat() {
     useEffect(() => {
         const messageInput = document.getElementById('message-input');
         const form = document.getElementById('form');
-    
+
         const handleSendMessage = (e) => {
             e.preventDefault();
             const message = messageInput.value;
@@ -153,9 +172,9 @@ export default function Chat() {
             console.log(username);
             messageInput.value = '';
         };
-    
+
         form.addEventListener('submit', handleSendMessage);
-    
+
         // Remove the event listener when the component unmounts
         return () => {
             form.removeEventListener('submit', handleSendMessage);
@@ -173,10 +192,34 @@ export default function Chat() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
+            {/* Username Change Modal */}
+            {isModalOpen && (
+                <div className='fixed inset-0 bg-gray-600 bg-opacity-50 z-50 flex justify-center items-center'>
+                    <div className='bg-white p-4 rounded-lg shadow-lg'>
+                        <form onSubmit={handleUsernameSubmit} className='flex flex-col items-center'>
+                            <input
+                                type='text'
+                                placeholder='Enter your username'
+                                value={newUsername}
+                                onChange={handleUsernameChange}
+                                className='font-pressStart border-2 border-gray-300 rounded py-2 px-4 focus:border-blue-500 focus:outline-none'
+                            />
+                            <button
+                                type='submit'
+                                className='font-pressStart bg-black hover:bg-blue-600 text-white font-bold px-6 py-2 rounded mt-2'
+                            >
+                                Set Username
+                            </button>
+                        </form>
+                        <button onClick={toggleModal} className="mt-4 text-red-500">Close</button>
+                    </div>
+                </div>
+            )}
 
             <div className='bg-gray-300 flex items-start justify-start tv-border'>
                 <div className='fixed w-4/5 h-4/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between'>
                     <div className="sm:hidden flex justify-between items-center">
+
                         <Link className='' href="/home">
                             <Image alt='BoboVision' className='' src={BoboVision} width={500} height={500}></Image>
                         </Link>
@@ -194,26 +237,32 @@ export default function Chat() {
                         <Web3Button className="ml-20" />
                     </div>
 
-                    <div className='z-0 grid-container absolute inset-x-0 bottom-10 py-10 h-4/5 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-y-auto'>
-                        <div id="message-container" className="col-span-4 w-4/5 max-w-6xl mb-4 overflow-y-auto flex flex-col">
+                    <div className='relative h-full grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-y-4 overflow-y-auto'>
+                        <div id="message-container" className="col-span-4 pb-8 sm:pb-14 w-full max-w-6xl overflow-y-auto flex flex-col" style={{ marginBottom: '100px' }}>
 
                         </div>
-                        <footer className="z-10 fixed bottom-0 w-full py-4">
+                        <footer className="absolute bottom-0 w-full bg-transparent">
                             <div className="flex justify-center">
-                                <form id="form" className="w-full sm:w-4/5 sm:max-w-6xl flex items-center">
+                                <form id="form" className="w-full flex flex-col space-y-1 items-center px-4">
                                     <input
                                         type="text"
                                         id="message-input"
-                                        className="z-20 font-pressStart w-full border-2 border-gray-300 rounded-l py-2 focus:border-blue-500 focus:outline-none"
+                                        className="font-pressStart w-full border-2 border-gray-300 rounded py-2 focus:border-blue-500 focus:outline-none"
                                         value={message}
                                         onChange={(e) => setMessage(e.target.value)}
                                     />
                                     <button
                                         type="submit"
                                         id="send-button"
-                                        className="font-pressStart bg-black hover:bg-blue-600 text-white font-bold px-6 py-2 rounded-r"
+                                        className="font-pressStart bg-black hover:bg-blue-600 text-white font-bold px-6 py-2 rounded w-full"
                                     >
                                         Send
+                                    </button>
+                                    <button
+                                        onClick={toggleModal}
+                                        className="bg-black hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-1/2"
+                                    >
+                                        Change Username
                                     </button>
                                 </form>
                             </div>
