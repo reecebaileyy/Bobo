eefaa2ddd0a87186e0b878c492c3c4ad6cd50ba1
Reece Bailey <80913719+reecebaileyy@users.noreply.github.com>
2023-12-26 04:16:51 -0800

    Update chat.js

--- website/src/pages/chat.js
+++ website/src/pages/chat.js
@@ -259,6 +259,7 @@ export default function Chat() {
                                         Send
                                     </button>
                                     <button
+                                        type="button" 
                                         onClick={toggleModal}
                                         className="bg-black hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-1/2"
                                     >
