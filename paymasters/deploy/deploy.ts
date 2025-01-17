f6190ac2caea40d61960876e24497e6797a9e605
Reece Bailey <80913719+reecebaileyy@users.noreply.github.com>
2025-01-16 21:10:02 -0800

    Error Fixed (Used System Teminal)

--- paymasters/deploy/deploy.ts
+++ paymasters/deploy/deploy.ts
@@ -3,9 +3,6 @@ import { deployContract } from "./utils";
 // An example of a basic deploy script
 // It will deploy a Greeter contract to selected network
 // as well as verify it on Block Explorer if possible for the network
-export default async function () {
-  const contractArtifactName = "Greeter";
-  const constructorArguments = ["Hi there!"];
-  
-  await deployContract(contractArtifactName, constructorArguments);
+export default async function () {  
+  await deployContract("MyPaymaster");
 }
